f5130465c7b7ef4a94063ec79c6ae570d902021a
03398351093 <03398351093@senacrs.edu.br>
2024-08-14 20:24:52 -0300

    Atividade foda

---
+++ Temperatura.py
@@ -0,0 +1,5 @@
+def main():
+    temp = float(input("Digite uma temperatura em graus e ela sera convertida para fahrenheit: "))
+    fahrenheit = (temp - 32) * 5/9
+    print(f"Sua temperatura em fahrenheit é, {fahrenheit}" )
+main()
